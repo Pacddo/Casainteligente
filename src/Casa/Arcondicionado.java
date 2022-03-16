9a21d445801d2d69da37310e0a54c7ac2bcccb1f
pacddo <douglas.pacheco@hotmail.com>
2022-03-16 02:58:44 -0300

    Alteraprogramaarcondicionado

--- src/Casa/Arcondicionado.java
+++ src/Casa/Arcondicionado.java
@@ -10,12 +10,24 @@ public class Arcondicionado {
     double tempambiente = 23;
     int onoff;
 
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
     void imprimirstatus() {
         System.out.println("Ar condicionado\nStatus: " + status + "\nTemperatura ar: " + temperatura +"" +
-                "\nTemperatura ambiente: " + tempambiente + "°C"+ "\n\nEscolha uma opção:\n1-Ligar/Desligar" +
-                "\n2-Alterar temperatura");
+                "\nTemperatura ambiente: " + tempambiente + "°C"+ "\n\nEscolha uma opção:\n0-Retornar\n" +
+                "1-Ligar/Desligar" + "\n2-Alterar temperatura");
+
         onoff =scan.nextInt();
+        if (onoff == 1 && status == "OFF"){
+           setStatus("ON");
+           imprimirstatus();
+        }else {setStatus("OFF");
+            imprimirstatus();}
+
+        }
     }
-}
+
 
 
